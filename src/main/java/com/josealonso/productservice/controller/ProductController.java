0743cb5f696719568b5927d270d34b6df0bf26a8
josealonso <jose.alonso.programmer@gmail.com>
2022-12-29 00:01:36 +0100

    Improved controller code, eliminating any warning.

--- src/main/java/com/josealonso/productservice/controller/ProductController.java
+++ src/main/java/com/josealonso/productservice/controller/ProductController.java
@@ -3,30 +3,43 @@ package com.josealonso.productservice.controller;
 import com.josealonso.productservice.dto.ProductResponse;
 import com.josealonso.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api/v1/product")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody ProductResponse productRequest) {
-        productService.createProduct(productRequest);
-    }
-
-    @GetMapping
+    @GetMapping("/products")
     @ResponseStatus(HttpStatus.OK)
     public List<ProductResponse> getAllProducts() {
         return productService.getAllProducts();
     }
+
+    @GetMapping({"/productId"})
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable("productId") String productId) {
+        return new ResponseEntity<>(productService.getProductById(productId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<HttpHeaders> createProduct(@RequestBody ProductResponse productResponse) {
+        ProductResponse savedProduct = productService.createProduct(productResponse);
+
+        HttpHeaders headers = new HttpHeaders();
+        // TODO add hostname to url
+        headers.add("Location", "/api/v1/product"+ savedProduct.getId());
+
+        return new ResponseEntity<HttpHeaders>(headers, HttpStatus.CREATED);
+    }
+
 }
 
 
