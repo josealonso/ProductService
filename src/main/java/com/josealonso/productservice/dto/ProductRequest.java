9510f154937844444d357f6b8de4489367230676
josealonso <jose.alonso.programmer@gmail.com>
2022-12-29 01:19:04 +0100

    Added fields related to the API version and to the modified date.

--- src/main/java/com/josealonso/productservice/dto/ProductRequest.java
+++ src/main/java/com/josealonso/productservice/dto/ProductRequest.java
@@ -4,15 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductRequest {
+    private Integer apiVersion;
     private String name;
     private String description;
     private BigDecimal price;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+
 }
--- src/main/java/com/josealonso/productservice/dto/ProductResponse.java
+++ src/main/java/com/josealonso/productservice/dto/ProductResponse.java
@@ -4,16 +4,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductResponse {
+    private Integer apiVersion;
     private String id;
     private String name;
     private String description;
     private BigDecimal price;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
 }
