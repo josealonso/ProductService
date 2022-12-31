dff3d48882f16b6b9a95d5ac012e78d2e6c5dc41
josealonso <jose.alonso.programmer@gmail.com>
2023-01-01 00:56:45 +0100

    Replaced domain entity.

---
+++ src/main/java/com/josealonso/productservice/domain/Product.java
@@ -0,0 +1,45 @@
+package com.josealonso.productservice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+    @Version
+    private Long version;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String productName;
+    private String productStyle;
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantity;
+}
