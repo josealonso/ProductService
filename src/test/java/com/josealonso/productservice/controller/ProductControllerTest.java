dcc6434a4d77e1715314a9be2f8952344dfdfa1d
josealonso <jose.alonso.programmer@gmail.com>
2023-01-02 14:23:51 +0100

    Documenting path parameters.

--- src/test/java/com/josealonso/productservice/controller/ProductControllerTest.java
+++ src/test/java/com/josealonso/productservice/controller/ProductControllerTest.java
@@ -1,8 +1,10 @@
 package com.josealonso.productservice.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.josealonso.productservice.domain.Product;
 import com.josealonso.productservice.dto.ProductRequest;
 import com.josealonso.productservice.dto.ProductResponse;
+import com.josealonso.productservice.repository.ProductRepository;
 import com.josealonso.productservice.service.ProductService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -19,10 +21,17 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs
@@ -30,14 +39,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ComponentScan(basePackages = "com.josealonso.productservice.mappers")
 class ProductControllerTest {
 
-    final String ENDPOINT = "/api/v1/product";
     static final UUID PRODUCT_ID = UUID.randomUUID();
     static ProductResponse productResponse;
+    final String ENDPOINT = "/api/v1/product";
     @Autowired
     MockMvc mockMvc;
 
     @MockBean
     ProductService productService;
+    @MockBean
+    ProductRepository productRepository;
 
     @Autowired
     ObjectMapper objectMapper;
@@ -52,6 +63,7 @@ class ProductControllerTest {
     @Test
     void getAllProducts() throws Exception {
         Mockito.when(productService.getAllProducts()).thenReturn(List.of(productResponse));
+        given(productRepository.findAll()).willReturn(List.of(getValidProduct()));
 
         mockMvc.perform(get(ENDPOINT + "/products")
                         .accept(MediaType.APPLICATION_JSON))
@@ -60,18 +72,21 @@ class ProductControllerTest {
 
     @Test
     void getProductById() throws Exception {
-        Mockito.when(productService.getProductById(PRODUCT_ID)).thenReturn(productResponse);
+        // Mockito.when(productService.getProductById(PRODUCT_ID)).thenReturn(productResponse);
+        given(productRepository.findById(any())).willReturn(Optional.of(getValidProduct()));
 
-        mockMvc.perform(get(ENDPOINT + "/" + PRODUCT_ID)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get(ENDPOINT + "/{productId}").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("/v1/product", pathParameters(
+                        parameterWithName("productId").description("UUID of the desired product to get.")
+                )));
     }
 
     @Test
     void createProduct() throws Exception {
         ProductResponse newProduct = getValidProductResponse();
         String newProductInJson = objectMapper.writeValueAsString(newProduct);
-        Mockito.when(productService.createProduct(newProduct)).thenReturn(newProduct);
+//        Mockito.when(productService.createProduct(newProduct)).thenReturn(newProduct);
 
         mockMvc.perform(post(ENDPOINT)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -120,6 +135,16 @@ class ProductControllerTest {
                 .upc(123123123L)
                 .build();
     }
+
+    Product getValidProduct() {
+        return Product.builder()
+                .id(PRODUCT_ID)
+                //.style("Book")
+                .price(new BigDecimal("2.99"))
+                .upc(123123123L)
+                .build();
+    }
+
     ProductRequest convertToProductRequest(ProductResponse productResponse) {
         return ProductRequest.builder()
                 .name(productResponse.getName())
