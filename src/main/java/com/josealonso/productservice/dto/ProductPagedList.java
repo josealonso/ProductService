b8e1c2aee2363aa9aa78f2212100a71a7c375bb5
josealonso <jose.alonso.programmer@gmail.com>
2023-01-03 20:20:00 +0100

    Added JsonCreator example.

--- src/main/java/com/josealonso/productservice/dto/ProductPagedList.java
+++ src/main/java/com/josealonso/productservice/dto/ProductPagedList.java
@@ -1,6 +1,10 @@
 package com.josealonso.productservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -9,6 +13,18 @@ public class ProductPagedList extends PageImpl<ProductResponse> {
 
     public ProductPagedList(List<ProductResponse> content, Pageable pageable, long total) {
         super(content, pageable, total);
+    }
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public ProductPagedList(@JsonProperty("content") List<ProductResponse> content,
+                            @JsonProperty("number") int number,
+                            @JsonProperty("size") int size,
+                            @JsonProperty("totalElements") int totalElements,
+                            @JsonProperty("pageable") JsonNode pageable,
+                            @JsonProperty("last") boolean last,
+                            @JsonProperty("totalPages") int totalPages) {
+
+        super(content, PageRequest.of(number, size), totalElements);
     }
 
     public ProductPagedList(List<ProductResponse> content) {
